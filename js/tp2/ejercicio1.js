60e2d94d439c08a95c0cf1c1c2463280826ccc6b
Paula Bejarano <paubejarano2@gmail.com>
2022-08-29 21:32:54 -0300

    inicie tp2 con ejercicio 3

---
+++ js/tp2/ejercicio1.js
@@ -0,0 +1,1 @@
+//1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
